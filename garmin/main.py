d8eb906f8694511c81e6f8978e457c5d59f53ef2
Sam Neirinck <sam@samneirinck.com>
2025-07-10 23:16:00 +0200

    Refactor garmin API endpoints, update routes to /v1, add workouts endpoints, and clean up unused code

--- garmin/main.py
+++ garmin/main.py
@@ -1,8 +1,6 @@
-import base64
 import garth
 from fastapi import Depends, FastAPI, HTTPException, status
 from pydantic import BaseModel
-import pprint
 from fastapi.security.api_key import APIKeyHeader
 
 app = FastAPI()
@@ -22,14 +20,9 @@ class AuthenticateResponse(BaseModel):
 
 def verify_api_key(api_key: str = Depends(garmin_key)):
     garth.client.loads(api_key)
-    # if api_key != expected_api_key:
-    #     raise HTTPException(
-    #         status_code=status.HTTP_403_FORBIDDEN,
-    #         detail="Invalid API Key",
-    #     )
 
 
-@app.post("/authenticate")
+@app.post("/v1/authenticate")
 def authenticate(request: AuthenticateRequest) -> AuthenticateResponse:
     result1, result2 = garth.login(request.email, request.password, return_on_mfa=True)
 
@@ -39,32 +32,16 @@ def authenticate(request: AuthenticateRequest) -> AuthenticateResponse:
     return AuthenticateResponse(token=token)
 
 
-def verify_token(token: str):
-    print(f"Verifying token: {token}")
-    if not token or token.strip() == "":
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Missing token",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    raise HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Invalid token",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-
-
-@app.get("/activities")
-def get_activities(token: str = Depends(verify_api_key)):
+@app.get("/v1/activities", dependencies=[Depends(verify_api_key)])
+def get_activities():
     activities = garth.client.connectapi(
         "activitylist-service/activities/search/activities"
     )
-    # garth.client.loads(token)
     return activities
 
 
-@app.get("/activities/{activity_id}")
-def get_activity(activity_id: str, token: str = Depends(verify_api_key)):
+@app.get("/v1/activities/{activity_id}", dependencies=[Depends(verify_api_key)])
+def get_activity(activity_id: str):
     activity = garth.client.connectapi(f"activity-service/activity/{activity_id}")
     if not activity:
         raise HTTPException(
@@ -74,11 +51,39 @@ def get_activity(activity_id: str, token: str = Depends(verify_api_key)):
     return activity
 
 
-@app.get("/user/profile")
-def get_user_profile(token: str = Depends(verify_api_key)):
-    return garth.UserProfile.get()
+@app.get("/v1/user/profile", dependencies=[Depends(verify_api_key)])
+def get_user_profile():
+    return garth.client.connectapi("/userprofile-service/socialProfile")
 
 
-@app.get("/user/settings")
-def get_user_settings(token: str = Depends(verify_api_key)):
-    return garth.UserSettings.get()
+@app.get("/v1/user/settings", dependencies=[Depends(verify_api_key)])
+def get_user_settings():
+    return garth.client.connectapi("/userprofile-service/userprofile/user-settings")
+
+
+@app.get("/v1/workouts", dependencies=[Depends(verify_api_key)])
+def get_workouts():
+    return garth.client.connectapi("workout-service/workouts")
+
+
+@app.get("/v1/workouts/{workout_id}", dependencies=[Depends(verify_api_key)])
+def get_workout(workout_id: str):
+    workout = garth.client.connectapi(f"workout-service/workout/{workout_id}")
+    if not workout:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Workout not found",
+        )
+    return workout
+
+
+@app.get("/v1/workouts/{workout_id}/{format}", dependencies=[Depends(verify_api_key)])
+def export_workout(workout_id: str):
+    workout = garth.download(f"workout-service/workout/{format}/{workout_id}")
+    if not workout:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Workout export not found",
+        )
+
+    return workout
